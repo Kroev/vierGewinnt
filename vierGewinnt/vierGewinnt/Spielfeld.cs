2d1127bb7edb854a8ffbf29b76f84bbe2023f05c
muell <muell@DESKTOP-TSFU0ST>
2017-05-29 16:41:11 +0200

    added methods testCoordinates, getByCoordinates and felderSetzen to Class Spielfeld

--- vierGewinnt/vierGewinnt/Spielfeld.cs
+++ vierGewinnt/vierGewinnt/Spielfeld.cs
@@ -8,12 +8,45 @@ namespace vierGewinnt
 {
     class Spielfeld
     {
-        private VGLabel[,] felder;
+        private int[,] felder;
 
         Spielfeld(int height, int width)
         {
-            this.felder = new VGLabel[height, width];
+            felder = new int [height, width];
         }
 
+        public int getByCoordinates(int x, int y)
+        {
+            if (this.testCoordinates(x, y) == false)
+            {
+                return null;
+            }
+            
+            else
+            {
+                return felder[x, y];
+            }
+        }
+
+        public int felderSetzen(int x, int y, int wert)
+        {
+            felder[x, y] = wert;
+        }
+
+        private bool testCoordinates(int x, int y)
+        {
+            if (x > (felder.GetLength(0) - 1))
+            {
+                return false;
+            }
+            else if (y > (felder.GetLength(1) - 1))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
