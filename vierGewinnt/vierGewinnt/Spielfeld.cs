321a63a513e040ec07e4aaac01ae3686836a6a2c
TheMieler <julianmueller12@yahoo.de>
2017-05-29 17:55:44 +0200

    Added Initialization for Array felder and commented Class Spielfeld

--- vierGewinnt/vierGewinnt/Spielfeld.cs
+++ vierGewinnt/vierGewinnt/Spielfeld.cs
@@ -13,13 +13,32 @@ namespace vierGewinnt
         Spielfeld(int height, int width)
         {
             felder = new int [height, width];
+
+            for (int x = 0; x < height; x++)
+            {
+                for(int y = 0; y < width; y++)
+                {
+                    felder[x, y] = 0;
+                }
+            }
         }
 
+        /* Gibt den Wert des jeweiligen Labels zurück
+         * 
+         * \param   x       Zeile des VGLabel's
+         * \param   y       Spalte des VGLabel's
+         * 
+         * \return  2       Label von Spieler 2
+         * \return  1       Label von Spieler 1
+         * \return  0       Label ist weiß
+         * \return  -1      Koordinaten sind außerhalb des Spielfeldes
+         * 
+        */
         public int getByCoordinates(int x, int y)
         {
             if (this.testCoordinates(x, y) == false)
             {
-                return null;
+                return -1; 
             }
             
             else
@@ -28,9 +47,26 @@ namespace vierGewinnt
             }
         }
 
+        /* Setzt den Wert des zugehörigen Label's fest
+         * 
+         * \param   x       Zeile des VGLabel's
+         * \param   y       Spalte des VGLabel's
+         * \param   wert    Wert des Labels 
+         * 
+         * \return  0       Wersetzung ist richtig verlaufen
+         * \return  -1      Koordinaten sind außerhalb des Spielfeldes
+        */
         public int felderSetzen(int x, int y, int wert)
         {
-            felder[x, y] = wert;
+            if(this.testCoordinates(x,y)== false)
+            {
+                return -1; 
+            }
+            else
+            {
+                felder[x, y] = wert;
+                return 0;
+            }
         }
 
         private bool testCoordinates(int x, int y)
