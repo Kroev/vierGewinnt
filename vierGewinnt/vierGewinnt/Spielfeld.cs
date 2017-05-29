865841c687d04ad0e2b69f7686e5fec029cff1f0
muell <muell@DESKTOP-TSFU0ST>
2017-05-29 12:11:14 +0200

    added Class Spielfeld

---
+++ vierGewinnt/vierGewinnt/Spielfeld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vierGewinnt
+{
+    class Spielfeld
+    {
+        private int[,] Felder;
+
+        Spielfeld(int height, int width)
+        {
+            
+        }
+    }
+}
