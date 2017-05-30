e6dc24532086ae0db8d200ad791f4a50e4eddf41
Kroev <s3808962@mail.zih.tu-dresden.de>
2017-05-30 21:49:01 +0200

    extended test for insert token into column until it is supposed to fail

--- vierGewinnt/vierGewinnt/Program.cs
+++ vierGewinnt/vierGewinnt/Program.cs
@@ -21,10 +21,21 @@ namespace vierGewinnt
         static void Main()
         {
             AllocConsole();
+
+            //test insert token into column
             Spielfeld spielfeld = new Spielfeld(5, 3);
             spielfeld.print();
             spielfeld.feldSetzen(0, 1);
             spielfeld.print();
+            spielfeld.feldSetzen(0, 2);
+            spielfeld.feldSetzen(0, 3);
+            spielfeld.feldSetzen(0, 4);
+            spielfeld.feldSetzen(0, 5);
+            spielfeld.print();
+            int test = spielfeld.feldSetzen(0, 6);
+            Console.WriteLine("trying to insert anotherone into column 0, if -2 -> OK: " + test);
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SpielForm());
