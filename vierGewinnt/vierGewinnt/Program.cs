0269bdf0aabdbc59472ded2870136783cbce42ba
Kroev <s3808962@mail.zih.tu-dresden.de>
2017-05-30 20:09:39 +0200

    in testbranch the application launches its own console so that poor kevin with broken desktop application can still debug :)

--- vierGewinnt/vierGewinnt/Program.cs
+++ vierGewinnt/vierGewinnt/Program.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
 
 namespace vierGewinnt
 {
     static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            AllocConsole();
             Spielfeld spielfeld = new Spielfeld(5, 3);
             spielfeld.print();
             Application.EnableVisualStyles();
