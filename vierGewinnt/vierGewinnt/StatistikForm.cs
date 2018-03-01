d439f23d8bdfa425867ab841f5aad189979bc7d5
SimonSaurier <shassmann96@gmail.com>
2018-03-01 12:24:37 +0100

    Testtabelle entfernt

---
+++ vierGewinnt/vierGewinnt/StatistikForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vierGewinnt
+{
+    public partial class StatistikForm : Form
+    {
+        public StatistikForm() : base()
+        {
+           
+            InitializeComponent();
+
+        }
+
+        private void StatistikForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
