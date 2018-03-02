534cfdd31c11a84379ea09e57a3bf6baf1774a56
kevin.seidel <kevin.seidel@accenture.com>
2018-03-02 11:00:51 +0100

    [dbconn] made getInstance static

--- vierGewinnt/vierGewinnt/DBConnector.cs
+++ vierGewinnt/vierGewinnt/DBConnector.cs
@@ -66,7 +66,7 @@ namespace vierGewinnt
             }
         }
 
-        public DBConnector getInstance()
+        public static DBConnector getInstance()
         {
             if ( instance == null )
             {
