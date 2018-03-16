97871b788a99bd7a3c8aad37725543d7326d8181
muell <muell@DESKTOP-TSFU0ST>
2018-03-16 08:44:51 +0100

    [Spielsteuerung]added Funktion for getplayer and getgames in statistik, added funktion Statistikumformen for get List of Hashtables in Datatable, added Gameupdate for set Data in Database

--- vierGewinnt/vierGewinnt/Spielsteuerung.cs
+++ vierGewinnt/vierGewinnt/Spielsteuerung.cs
@@ -15,7 +15,7 @@ namespace vierGewinnt
         private Spieler rot; //Spieler 2 von Wert her
         private int akt;
         private int spielende; //0 -> spiel läuft; akt -> gewonnen, -2 -> remis
-        private DataTable hashtabelle;
+        
 
         /**
          * Erzeugt eine neue Spielsteuerung mit einem Spielfeld mit der angegebenen Höhe
@@ -60,18 +60,14 @@ namespace vierGewinnt
                     this.spielende = this.akt;
                     //elorechnung
                     elorechnung(this.spielende);
-                    DBConnector con = DBConnector.getInstance();
-                    con.updatePlayerElo(gelb.Name, gelb.Elo);
-                    con.updatePlayerElo(rot.Name, rot.Elo);
+                    Gameupdate(gewinn);
                 }
                 else if ( gewinn == -2 )
                 {
                     this.spielende = -2;
                     //elorechnung
                     elorechnung(this.spielende);
-                    DBConnector con = DBConnector.getInstance();
-                    con.updatePlayerElo(gelb.Name, gelb.Elo);
-                    con.updatePlayerElo(rot.Name, rot.Elo);
+                    Gameupdate(gewinn);
                 }
                 else
                 {
@@ -242,21 +238,39 @@ namespace vierGewinnt
 
         }
 
-        public static DataTable Statistikholen()
+        public static DataTable StatistikGetPlayer()
         {
             DataTable hashtabelle = new DataTable();
-            hashtabelle.Columns.Add("name", typeof(string));
-            hashtabelle.Columns.Add("elo", typeof(int));
             List<Hashtable> hashliste = new List<Hashtable>();
             DBConnector con = DBConnector.getInstance();
             hashliste = con.getAllPlayers();
+            hashtabelle = StatistikUmformen(hashliste);
+            return hashtabelle;
+        }
+
+        public static DataTable StatistikGetGame()
+        {
+            DataTable hashtabelle = new DataTable();
+            List<Hashtable> hashliste = new List<Hashtable>();
+            DBConnector con = DBConnector.getInstance();
+            hashliste = con.getGames();
+            hashtabelle = StatistikUmformen(hashliste);
+            return hashtabelle;
+        }
+
+        private static DataTable StatistikUmformen(List<Hashtable> hashliste)
+        {
+            DataTable hashtabelle = new DataTable();
             if (hashliste != null)
             {
+                foreach (string key in hashliste[0].Keys)
+                {
+                    hashtabelle.Columns.Add(key, typeof(string));
+                }
                 foreach (Hashtable hashes in hashliste)
                 {
-                    hashtabelle.Rows.Add(hashes["name"], hashes["elo"]);
+                    hashtabelle.Rows.Add(hashes);
                 }
-
             }
             return hashtabelle;
         }
@@ -293,6 +307,14 @@ namespace vierGewinnt
             }
 
             return 0;
+        }
+
+        public void Gameupdate(int result)
+        {
+            DBConnector con = DBConnector.getInstance();
+            con.updatePlayerElo(gelb.Name, gelb.Elo);
+            con.updatePlayerElo(rot.Name, rot.Elo);
+            con.recordGame(gelb.Name, rot.Name, result);
         }
 
         public int Spielende
